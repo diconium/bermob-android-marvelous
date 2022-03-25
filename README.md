ac6d17221efe8268c2e360c6b5c5223751973618
Ronaldo Pace <ronaldo.pace@diconium.com>
2022-03-25 15:38:44 +0100

    Change build scripts to kts update to latest gradle applied refreshVersions plugin

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Marvelous Android
 
  An Android app to explore the Marvel API.
+
+## Setup & Building
+
+The Android Studio built-in IDE is not very smart on understanding the kts build scripts.
+It's advisable to change on Preferences -> Build, Execution, Deployment -> Gradle to Termurin.
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:_")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+val clean by tasks.registering(Delete::class) {
+    group = "build"
+    delete(buildDir)
+}
